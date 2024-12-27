4501b179feba55fb7323f773410b35b9e389418a
Pablo Fonseca <pablofonseca777@gmail.com>
2024-12-27 22:09:37 +0000

    finish 4th stage

--- app/server.go
+++ app/server.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"bytes"
+	"bufio"
 	"fmt"
 	"net"
 	"os"
@@ -31,8 +31,16 @@ func main() {
 		fmt.Println("Error reading data: ", err.Error())
 	}
 
-	requestData := bytes.Split(buf, []byte("\r\n"))
-	requestLine := string(requestData[0])
+	scanner := bufio.NewScanner(strings.NewReader(string(buf[:])))
+
+	var requestLine string
+
+	if scanner.Scan() {
+		requestLine = scanner.Text()
+	}
+
+	headers := parseHeaders(scanner)
+
 	url := strings.Split(requestLine, " ")[1]
 
 	if url == "/index.html" || url == "/" {
@@ -41,7 +49,30 @@ func main() {
 		resp, _ := strings.CutPrefix(url, "/echo/")
 
 		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(resp), string(resp))))
+	} else if url == "/user-agent" {
+		userAgent := headers["user-agent"]
+		conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(userAgent), userAgent)))
 	} else {
 		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 	}
+}
+
+func parseHeaders(scanner *bufio.Scanner) map[string]string {
+	headers := make(map[string]string)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if line == "" {
+			break
+		}
+
+		parts := strings.SplitN(line, ": ", 2)
+		if len(parts) == 2 {
+			key, value := parts[0], parts[1]
+			headers[strings.ToLower(key)] = value
+		}
+	}
+
+	return headers
 }
